71d3b891993786c932cdf87b1b80c936e94949a3
Jackson <jackson@192.168.1.3>
2015-04-13 23:42:59 +0800

    update

---
+++ QtServer/QtServer-build/CMakeFiles/QtServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QtServer.dir/main.cpp.o"
+  "QtServer.pdb"
+  "QtServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QtServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
